cb5b32ebb5a1e402058f2b0c0eef364251a26e7e
liliuwli <861917988@qq.com>
2020-06-08 23:51:14 +0800

    first day

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # flutter_book
 一款可以自定义来源阅读网络内容的工具
+
+第一日：
+完成dark语言flutter UIdemo
+
+成果:
++ 了解Flutter应用程序的基本结构  
++ 查找和使用packages来扩展功能  
++ 使用热重载加快开发周期  
++ 如何实现有状态的widget  
++ 如何创建一个无限的、延迟加载的列表  
+
+坑：
++ Column嵌套无限高度ListView不能自适应，解决方案=》利用Expanded嵌套，类似清除浮动
---
+++ reader/lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Reader',
+      home: new App(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  createState() => new AppState();
+}
+
+class AppState extends State<App> {
+  @override
+  //控制TextField 焦点的获取与关闭
+  FocusNode focusNode = new FocusNode();
+
+  //build被setState触发
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('阅读'),
+        ),
+        body: new Column(
+          children: [
+            //初始化一个盒子容纳搜索框
+            new Container(
+              child: new TextField(
+                decoration: InputDecoration(
+                    hintText:'输入书籍或作者'
+                ),
+                focusNode: focusNode,
+                autofocus: false,
+                onTap: _clickSearch,
+              ),
+              margin: const EdgeInsets.only(right: 20,left: 20,top: 10,bottom: 10),
+            ),
+            Expanded(
+                //嵌套解决 ListView高度超标问题
+                child:new Container(
+                  child:new ListView.builder(
+                    padding: const EdgeInsets.all(16.0),
+                    shrinkWrap: true,
+                    itemCount: 10,
+                    itemBuilder:(context,i){
+                      if(i<10){
+                        return new BookItem('https://www.booktxt.com/files/article/image/28/28228/28228s.jpg','小阁老','三戒大师','新书感言','第二百四十五章 火并',null);
+                      }
+                    },
+                  ),
+                )
+            )
+          ],
+        ),
+    );
+  }
+
+  void _clickSearch(){
+    focusNode.unfocus();
+    print("点击成功，但是并未触发小键盘，后面改成路由跳转");
+    return;
+  }
+}
+
+//单行操作
+class BookItem extends StatelessWidget {
+  @override
+  String img;
+  String name;
+  String author;
+  String readmark;
+  String lastChapter;
+  Map Source;
+
+  BookItem(this.img,this.name,this.author,this.readmark,this.lastChapter,this.Source);
+
+  final titlefont = const TextStyle(fontSize: 16.0,height: 1);
+  final otherfont = const TextStyle(fontSize: 10,height: 1);
+
+  Widget build(BuildContext context) {
+    return new Container(
+        child:new Row(
+          children: [
+            //img
+            Expanded(
+                child: new Container(
+                  child: new Image.network(this.img),
+                  margin: EdgeInsets.only(right: 10,bottom: 10,top: 10),
+                ),
+                flex:2
+            ),
+            Expanded(
+                child: new Column(
+                  children: [
+                    //标题
+                    new Row(
+                      children: [
+                        new Icon(Icons.book,color: Colors.black26,),
+                        new Text(
+                            this.name,
+                            style: titlefont,
+                        )
+                      ],
+                    ),
+                    //作者
+                    new Row(
+                      children: [
+                        new Icon(Icons.account_circle,color: Colors.black26,),
+                        new Text(
+                          this.author,
+                          style: otherfont,
+                        )
+                      ],
+                    ),
+                    //已读
+                    new Row(
+                      children: [
+                        new Icon(Icons.access_time,color: Colors.black26,),
+                        new Text(
+                          this.readmark,
+                          style: otherfont,
+                        )
+                      ],
+                    ),
+                    //更新
+                    new Row(
+                      children: [
+                        new Icon(Icons.access_alarm,color: Colors.black26,),
+                        new Text(
+                          this.lastChapter,
+                          style: otherfont,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                flex:4
+            ),
+            Expanded(
+                child: new Container(
+                  child: new Icon(Icons.filter_9_plus,color: Colors.red,),
+                ),
+                flex:1
+            ),
+            new Padding(
+                padding: EdgeInsets.all(26),
+                child: new Divider()
+            )
+          ],
+        ),
+        decoration: BoxDecoration(
+            border:Border(
+                bottom:BorderSide(width: 1,color: Colors.black26)
+            )
+        ),
+    );
+  }
+}
